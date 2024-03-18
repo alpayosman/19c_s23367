b79d73a790bd95e1daa19c33e57d37ff965006e8
GlockOs <osmanalpay898@gmail.com>
2024-03-18 12:23:51 +0100

    Completed Task 8

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,5 +1,16 @@
+from main import RaiseError
+
+
 class SquareGenerator:
     def generate_the_squares(self, start, end):
         if end<start:
             return []
         return [x**2 for x in range(start, end + 1)]
+# Task 8
+class CubicGenerator(SquareGenerator):
+    def generate_squares(self, start, end):
+
+        if end < start:
+            raise RaiseError(f"End must be greater than start")
+        return [x ** 3 for x in range(start, end + 1)]
+
