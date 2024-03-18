7ff9f8c10c65bdba2716c1954e5cc2f6438b8813
GlockOs <osmanalpay898@gmail.com>
2024-03-18 11:40:47 +0100

    Completed Task 3 and Task 5

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ def generate_square(start, end):
 start = int(input("Enter the start of the range: "))
 end = int(input("Enter the end of the range: "))
 print(generate_square(start, end))
+
+
+# Task 3
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            return [] # Task 5 handle the smaller end value then range
+        return [x**2 for x in range(start, end + 1)]
+
+
+square_gen = SquareGenerator()
+
+squares = square_gen.generate_squares(1, 10)
+print(squares)
