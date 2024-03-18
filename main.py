b444ec3e06f0f501b55ce7ac9bba14f6ef941f79
GlockOs <osmanalpay898@gmail.com>
2024-03-18 11:38:32 +0100

    Completed Task 2

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # Task 1
 
-squares = [x**2 for x in range(1, 11)]
+squares = [x ** 2 for x in range(1, 11)]
 print(squares)
+
+
+# Task 2
+
+def generate_square(start, end):
+    return [x ** 2 for x in range(start, end + 1)]
+
+
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+print(generate_square(start, end))
